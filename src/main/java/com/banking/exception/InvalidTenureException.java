24c9cb68938b05bd8df1de67ac696c872f0d3e0b
kumarmangalam <kmrmnglm@gmail.com>
2026-06-07 21:32:04 +0530

    Added InvalidTenureException

---
+++ src/main/java/com/banking/exception/InvalidTenureException.java
@@ -0,0 +1,7 @@
+package com.banking.exception;
+
+public class InvalidTenureException extends RuntimeException {
+    public InvalidTenureException(String message) {
+        super(message);
+    }
+}
