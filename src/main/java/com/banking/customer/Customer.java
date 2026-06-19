e33ee85be2f66d38eadb1345d7eca31ba70809b2
kumarmangalam <kmrmnglm@gmail.com>
2026-06-19 14:31:40 +0530

    removed : redundant setters from customer class.

--- src/main/java/com/banking/customer/Customer.java
+++ src/main/java/com/banking/customer/Customer.java
@@ -25,43 +25,18 @@ public class Customer {
     public String getCustomerId() {
         return customerId;
     }
-
-    public void setCustomerId(String customerId) {
-        this.customerId = customerId;
-    }
-
     public String getFirstName() {
         return firstName;
     }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public String getLastName() {
         return lastName;
     }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public String getEmail() {
         return email;
     }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public List<Account> getAccounts() {
         return accounts;
     }
-
-    public void setAccounts(List<Account> accounts) {
-        this.accounts = accounts;
-    }
-
     public void addAccount(Account account) {
         accounts.add(account);
     }
